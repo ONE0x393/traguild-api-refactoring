34f5940fbec7c57cc6be887c12e392feaffef572
jhw0900 <jhw0900@gmail.com>
2024-07-26 14:47:32 +0900

    IC19-13 * CURDATE TZ 오류 수정(2)

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,11 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 13306,
     dialect: 'mysql',
-    timezone: 'Etc/GMT-9',
+    dateStrings: 'date',
+    timezone: "+09:00", // DB에 저장할 때 시간 설정
+    dialectOptions: {
+        timezone: "+09:00", // DB에서 가져올 때 시간 설정
+    }
 });
 
 module.exports = sequelize;
