d041077db5bdac4aefa2be823be7980aadb07d48
jhw0900 <jhw0900@gmail.com>
2024-07-26 13:57:30 +0900

    IC19-13 * CURDATE TZ 오류 수정

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 13306,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: 'Etc/GMT-9',
 });
 
 module.exports = sequelize;
