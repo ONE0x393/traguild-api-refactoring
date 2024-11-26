31a63bc9feec865515d81cb2602385182b8469ae
이진우 <dlwlsdn8864@naver.com>
2024-11-26 09:18:31 +0900

    -4. 시험값 수정

--- src/models/dummy/insertRequestApplicant.js
+++ src/models/dummy/insertRequestApplicant.js
@@ -3,25 +3,25 @@ const RequestApplicant = require('@src/models/RequestApplicant');
 const insertRequestApplicant = async () => {
     return RequestApplicant.bulkCreate([
         {
-            "request_idx": "1",
-            "applicant_idx": "1",
+            "request_idx": 1,
+            "applicant_idx": 2,
             "applicant_state": "수락"
         },{
-            "request_idx": "1",
-            "applicant_idx": "2",
+            "request_idx": 2,
+            "applicant_idx": 7,
             "applicant_state": "취소",
             "is_canceled": "1"
         },{
-            "request_idx": "1",
-            "applicant_idx": "3",
+            "request_idx": 2,
+            "applicant_idx": 8,
             "applicant_state": "대기"
         },{
-            "request_idx": "5",
-            "applicant_idx": "3",
+            "request_idx": 6,
+            "applicant_idx": 3,
             "applicant_state": "거절"
         },{
-            "request_idx": "3",
-            "applicant_idx": "5",
+            "request_idx": 6,
+            "applicant_idx": 5,
             "applicant_state": "수락"
         },
     ]);
