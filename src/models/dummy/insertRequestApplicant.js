85a64350a839353bb0970f76c740e007e545618e
이진우 <dlwlsdn8864@naver.com>
2024-11-26 09:30:09 +0900

    -4. 시험값 수정2

--- src/models/dummy/insertRequestApplicant.js
+++ src/models/dummy/insertRequestApplicant.js
@@ -5,7 +5,7 @@ const insertRequestApplicant = async () => {
         {
             "request_idx": 1,
             "applicant_idx": 2,
-            "applicant_state": "수락"
+            "applicant_state": "대기"
         },{
             "request_idx": 2,
             "applicant_idx": 7,
@@ -18,11 +18,55 @@ const insertRequestApplicant = async () => {
         },{
             "request_idx": 6,
             "applicant_idx": 3,
-            "applicant_state": "거절"
+            "applicant_state": "대기"
         },{
             "request_idx": 6,
             "applicant_idx": 5,
-            "applicant_state": "수락"
+            "applicant_state": "대기"
+        },{
+            "request_idx": 4,
+            "applicant_idx": 1,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 5,
+            "applicant_idx": 32,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 23,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 45,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 55556,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 34556,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 2346,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 12684,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 43244,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 87554,
+            "applicant_state": "대기"
+        },{
+            "request_idx": 6,
+            "applicant_idx": 3859,
+            "applicant_state": "대기"
         },
     ]);
 }
